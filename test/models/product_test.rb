78a96109fa6d8ce68b28df5efb715d4de3ff4a2e
get4137 <get4137@gmail.com>
2019-08-21 18:15:24 +0300

    add annotates

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
